99aa1cc9a06922350bce185691c22d3995587feb
James <mirageapache@gmail.com>
2022-12-05 21:26:20 +0800

    Set route and add views, Import Bootstrap

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 
+//view engine
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+//static files
+app.use(express.static('public'))
+
+// index page
 app.get('/', (req,res) => {
-  res.send('initialize success')
+  // res.send('initialize success')
+  res.render('index', {stylesheet: 'index.css'})
 })
+
 
 
 app.listen(port, () => {
