91559abe67e77687a2bad2069c95486d42ea7cd4
James <mirageapache@gmail.com>
2022-12-05 22:15:50 +0800

    get json data and render index page and show page

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-
+const restaurant_list = require('./restaurant.json') //import restaurant.json data
+  
 //view engine
 app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
@@ -12,10 +13,14 @@ app.use(express.static('public'))
 
 // index page
 app.get('/', (req,res) => {
-  // res.send('initialize success')
-  res.render('index', {stylesheet: 'index.css'})
+  res.render('index', {stylesheet: 'index.css',data: restaurant_list.results})
 })
 
+app.get('/restaurants/:restaurant_id', (req,res) => {
+  const show_data = restaurant_list.results.filter(item => item.id === Number(req.params.restaurant_id))
+  console.log(show_data)
+  res.render('show',{stylesheet: 'show.css', data: show_data[0]})
+})
 
 
 app.listen(port, () => {
